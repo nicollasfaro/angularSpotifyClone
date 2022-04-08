11c9e86a91ad69cc925d97f5d122f0ae556fb9b9
Nicollas Faro <nicollasciuldin@gmail.com>
2022-04-08 20:17:32 -0300

    atualização

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { LoginRotas } from './login.routes';
 
 
 
@@ -9,7 +11,9 @@ import { LoginComponent } from './login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(LoginRotas)
+
   ]
 })
 export class LoginModule { }
