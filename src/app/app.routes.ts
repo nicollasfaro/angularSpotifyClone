228c2cded0ef79cdfcc6016701ce136122415610
Nicollas Faro <nicollasciuldin@gmail.com>
2022-04-08 19:05:45 -0300

    atualização até o momento

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+
+export const AppRotas: Routes = [
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module')
+    }
+]
